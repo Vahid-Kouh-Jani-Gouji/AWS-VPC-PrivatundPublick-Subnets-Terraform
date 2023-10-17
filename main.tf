4028d4bf3feef5ce995f9a08728e5eb49c56e8fc
vahid koohjani <vk1604@yahoo.com>
2023-10-17 16:55:21 +0200

    privat subnets

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Alle Locals werden innerhalb des locals Block definiert
 locals {
   az_a = "${var.region}a" # eu-central-1a
   az_b = "${var.region}b"
@@ -114,6 +115,10 @@ resource "aws_security_group" "sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    name="TF-SG"
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
@@ -122,6 +127,10 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_a.id
   vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    name="TF-test-a"
+  }
 }
 
 resource "aws_instance" "test-b" {
@@ -129,6 +138,9 @@ resource "aws_instance" "test-b" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_b.id
   vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    name="TF-test-b"
+  }
 }
 
 resource "aws_instance" "test-c" {
@@ -136,4 +148,133 @@ resource "aws_instance" "test-c" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_c.id
   vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    name="TF-test-c"
+  }
+}
+
+# Erstelle jetzt in jedem AZ ein weiteres privates Subnetz. Verwende hierfür ein Nat Gateway.
+
+# Nat Gateway in AZ A
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_a.id
+  subnet_id     = aws_subnet.subnet_a.id
+  tags = {
+    name="TF-nat-a"
+  }
+}
+
+# Elastic IP für NAT Gateway in AZ A
+resource "aws_eip" "nat_a" {
+  domain           = "vpc"
+}
+
+# Route Table für privates Subnetz in AZ A
+resource "aws_route_table" "private_rt_a" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+}
+
+# Subnetz in AZ A für private Instanzen
+resource "aws_subnet" "subnet_a_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24" # Passen Sie die CIDR-Block-Definition entsprechend an
+  availability_zone = local.az_a
+  map_public_ip_on_launch = false # Keine öffentlichen IPs für private Subnetze
+
+  tags = {
+    Name = "TF Subnet A (Private)"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_a" {
+  subnet_id      = aws_subnet.subnet_a_private.id
+  route_table_id = aws_route_table.private_rt_a.id
+}
+
+
+
+# Nat Gateway in AZ B
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_b.id
+  subnet_id     = aws_subnet.subnet_b.id
+}
+
+# Elastic IP für NAT Gateway in AZ B
+resource "aws_eip" "nat_b" {
+  domain           = "vpc"
+}
+
+# Route Table für privates Subnetz in AZ B
+resource "aws_route_table" "private_rt_b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+}
+
+# Subnetz in AZ B für private Instanzen
+resource "aws_subnet" "subnet_b_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.5.0/24" # Passen Sie die CIDR-Block-Definition entsprechend an
+  availability_zone = local.az_b
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "TF Subnet B (Private)"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_b" {
+  subnet_id      = aws_subnet.subnet_b_private.id
+  route_table_id = aws_route_table.private_rt_b.id
+}
+
+# Wiederholen Sie die oben genannten Schritte für die AZ C, um private Subnetze und NAT Gateways in AZ C zu erstellen.
+
+# Beachten Sie, dass Sie auch die Sicherheitsgruppen für die privaten Instanzen entsprechend konfigurieren müssen, um den Kommunikationsbedarf zu erfüllen.
+
+# Stellen Sie sicher, dass Sie die Variablen und andere Konfigurationen entsprechend anpassen, um Ihre speziellen Anforderungen und Präferenzen zu berücksichtigen.
+
+resource "aws_nat_gateway" "nat_c" {
+  allocation_id = aws_eip.nat_c.id
+  subnet_id     = aws_subnet.subnet_c.id
+}
+
+# Elastic IP für NAT Gateway in AZ c
+resource "aws_eip" "nat_c" {
+  domain           = "vpc"
+}
+
+# Route Table für privates Subnetz in AZ c
+resource "aws_route_table" "private_rt_c" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_c.id
+  }
+}
+
+# Subnetz in AZ c für private Instanzen
+resource "aws_subnet" "subnet_c_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.6.0/24" # Passen Sie die CIDR-Block-Definition entsprechend an
+  availability_zone = local.az_c
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "TF Subnet B (Private)"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_c" {
+  subnet_id      = aws_subnet.subnet_c_private.id
+  route_table_id = aws_route_table.private_rt_c.id
 }
