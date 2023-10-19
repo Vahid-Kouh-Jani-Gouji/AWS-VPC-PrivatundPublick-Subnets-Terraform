e10b64b98f9be9c154753156dd89541f3f068796
vahid koohjani <vk1604@yahoo.com>
2023-10-19 16:56:37 +0200

    add dev-variables.tfvars

---
+++ dev-variables.tfvars
@@ -0,0 +1,5 @@
+region = "eu-central-1"
+
+vpc_cidr    = "10.0.0.0/16"
+subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+vzs         = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
   type    = string         # Welcher Datentyp ist die Variable?
-  default = "eu-central-1" # Welchen Wert hat die Variable, wenn nichts angegeben wird?
+  
 }
 
 variable "vzs" {
-  description = "Variable Zone"
+  description = "available Zone"
   type        = list(string)
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+
 
 }
 
 variable "vpc_cidr" {
   type        = string
   description = "vpc segement"
-  default     = "10.0.0.0/16"
+  
 }
 
 
 variable "subnet_cidr" {
   description = "subnet segement"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  
 }
